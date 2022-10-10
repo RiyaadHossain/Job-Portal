079a0dd9a93ebd867b6bcd8da447bfd81f56f9cb
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-10 07:52:50 +0600

    file structure defined

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000
+const colors = require('colors');
+const dotenv = require('dotenv');
 const app = express()
 
+dotenv.config()
+
+// Database Connect
+require('./src/Config/dbConfig')
+
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
+    console.log(`Server is running on PORT: ${PORT}`.bold.yellow);
 })
--- src/Config/dbConfig.js
+++ src/Config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
-        console.log('DB connect successfully')
+        console.log('Database connect successfully'.bold.cyan)
     })
     .catch(err => {
         console.log(err);
